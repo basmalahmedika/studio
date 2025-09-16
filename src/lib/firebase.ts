435609c1856504ef6351e5f2dac643148badf56e
Basmalah Medika <klinikrawatinapbasmalahmedika@gmail.com>
2025-09-16 05:34:26 +0000

    bagaimana jika menggunakan vercel

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbiJ8FtbYrM9PECDiixI_GfrpxGrzbIxM",
-  authDomain: "apotekbasmalahv3.firebaseapp.com",
-  databaseURL: "https://apotekbasmalahv3-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "apotekbasmalahv3",
-  storageBucket: "apotekbasmalahv3.appspot.com",
-  messagingSenderId: "902423755017",
-  appId: "1:902423755017:web:b814c7fdc4b450bd8ebbd8",
-  measurementId: "G-3CPKM1FLKP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
